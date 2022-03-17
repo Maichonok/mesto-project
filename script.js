65056a37c431e92977aa77415fd841e8b026bae3
Maichonok <mayamatueva@gmail.com>
2022-03-17 13:11:10 +0100

    edit fields of form, save an information on the page, popup edit profile opened smoothly

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+ // open and close popup Edit profile
 let editButton = document.querySelector('.profile__edit-btn');
 
 function openPopup() {
@@ -6,6 +7,8 @@ function openPopup() {
 }
 
 editButton.addEventListener('click', openPopup);
+editButton.addEventListener('click', addInformation);
+
 
 let closeButton = document.querySelector('.popup__close-btn');
 
@@ -15,3 +18,40 @@ function closePopup() {
 }
 
 closeButton.addEventListener('click', closePopup);
+
+
+// add an information in form fields
+function addInformation() {
+  // получили доступ к input (находим поля формы в DOM)
+  let nameField = document.querySelector('#profile__name');
+  let textField = document.querySelector('#profile__text');
+
+  // получили доступ к заголовку и тексту профиля
+  let profileName = document.querySelector('.profile__name');
+  let profileText = document.querySelector('.profile__text');
+
+  // установили новое значение inputs
+  nameField.value = profileName.textContent;
+  textField.value = profileText.textContent;
+}
+
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__form');
+
+formElement.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+
+  let nameField = document.querySelector('#profile__name');
+  let textField = document.querySelector('#profile__text');
+  let profileName = document.querySelector('.profile__name');
+  let profileText = document.querySelector('.profile__text');
+
+  profileName.textContent= nameField.value;
+  profileText.textContent = textField.value;
+
+  // console.log('Форма отправлена');
+
+  closePopup();
+});
+
+
